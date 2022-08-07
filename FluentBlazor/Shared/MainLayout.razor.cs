116b68f3aa24b97ba8d48734c5e4d8cb7cfba947
Andrey Tedeev <andrey.tedeev@outlook.com>
2022-08-07 16:19:40 +0700

    separate cs source for layout add miner page

---
+++ FluentBlazor/Shared/MainLayout.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace FluentBlazor.Shared
+{
+    public partial class MainLayout : LayoutComponentBase
+    {
+        private readonly Dictionary<string, string> _menuItems = new()
+        {
+            { "Home", "/"},
+            { "Counter", "/counter"},
+            { "Fetch", "/fetchdata"},
+            { "Miner", "/miner"},
+        };
+
+        public Dictionary<string, string> MenuItems => _menuItems;
+    }
+}
