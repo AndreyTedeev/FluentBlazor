0059142a9fc7b708a5ad2f2f16de5df5d56b685f
Andrey Tedeev <andrey.tedeev@outlook.com>
2022-08-07 17:42:40 +0700

    add basic miner board

---
+++ FluentBlazor/Pages/Miner.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace FluentBlazor.Pages
+{
+    public partial class Miner : ComponentBase
+    {
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
